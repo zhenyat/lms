52092b89d3da2facf5a39f1776a267875d7541b6
Zhenya Telyukov <telyukov@gmail.com>
2017-08-28 20:23:32 +0300

    carousel updates

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,21 +1,24 @@
 /*
- * Source:  https://www.bootply.com/132400
+ * Source:  https://bootsnipp.com/snippets/l1Zqa
  */
+
 function carousel_multi_items() {
-  $('.carousel[data-type="multi"] .item').each(function(){
+  $('#myCarousel').carousel({
+    interval: 4000
+  });
+
+  $('.carousel .item').each(function(){
     var next = $(this).next();
     if (!next.length) {
       next = $(this).siblings(':first');
     }
     next.children(':first-child').clone().appendTo($(this));
 
-    for (var i=0;i<2;i++) {
-      next=next.next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
-      }
-
-      next.children(':first-child').clone().appendTo($(this));
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
+    }
+    else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
     }
   });
-};
+}
